8f5cc1e8653f9c8ef58cdaae80b8b28bdbfb8f04
freed0m <nazar.garaev.07@mail.ru>
2022-04-26 15:16:58 +0500

    Попробовал сделать свой стиль

--- static/script/script.js
+++ static/script/script.js
@@ -5,20 +5,20 @@ function update_chat() {
             var h4;
             if (element.type == 'msg') {
                 h4 = jQuery('<h4>', {
-                    class: 'message rounded bg-info w-25 container',
+                    class: 'message rounded bg-light w-25 container',
                     text: element.message,
                 }).appendTo('#chat_box');
             }
             else {
                 h4 = jQuery('<h4>');
                 jQuery('<img>', {
-                    class: 'message rounded w-25 container bg-opacity-10',
+                    class: 'message rounded w-25 container',
                     src: element.message,
                     alt: ''
                 }).appendTo(h4);
                 h4.appendTo('#chat_box');
             }
-            h4.append(`<button class="btn-remove" data-id=${element.id}></button>`);
+            h4.append(`<button class="btn-remove" data-id=${element.id}><img class="del_img" src="/static/css/images/btn-remove.png" width="30" height="30"></button>`);
         });
         $('button.btn-remove').click(function () {
             event.preventDefault();
